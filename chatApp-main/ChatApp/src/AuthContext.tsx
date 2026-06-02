90ee5591639c10f8cbf18e02a0bc6bd399290abd
Mohan Singh <146318390+mohan1389@users.noreply.github.com>
2026-06-03 03:30:57 +0530

    Refactor backend URL assignment in AuthContext
    Remove fallback URL for backend configuration.

--- chatApp-main/ChatApp/src/AuthContext.tsx
+++ chatApp-main/ChatApp/src/AuthContext.tsx
@@ -141,7 +141,8 @@ export const AuthContext = createContext<any>('');
 function AuthContextProvider({children}: {children: React.ReactNode}) {
     // const backEndURL = import.meta.env.VITE_BACKEND_URL;
     // const backEndURL = "http://localhost:5000";
-    const backEndURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+    // const backEndURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+    const backEndURL = import.meta.env.VITE_BACKEND_URL;
     console.log("BACKEND URL =", backEndURL);
     const [signedIn, setIsSignedIn] = useState<boolean>(false);
     const [userInfo, setUserInfo] = useState<any>();
